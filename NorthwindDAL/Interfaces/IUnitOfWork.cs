1c0c65cbfbf6863b1f8725c0645f15f68aa0a78e
Yauheniya Strelkova <Yauheniya_Strelkova@epam.com>
2020-04-14 23:31:54 +0300

    added DAL project

---
+++ NorthwindDAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NorthwindDAL.Entities;
+
+namespace NorthwindDAL
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Customer> Customers { get; }
+        IRepository<Order> Orders { get; }
+        IRepository<Product> Products { get; }
+        IRepository<Supplier> Suppliers { get; }
+        IRepository<Shipper> Shippers { get; }
+        IRepository<Territory> Territories { get; }
+        void Save();
+    }
+}
